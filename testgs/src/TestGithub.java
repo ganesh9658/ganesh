c1bc1e59c73c17a182ef577e61aac7f3f92912a7
Dinesh Saivolla <Dinesh Saivolla@DESKTOP-UGDET12>
2023-12-26 17:37:42 +0530

    # WARNING: head commit changed in the meantime
    project added

---
+++ testgs/src/TestGithub.java
@@ -0,0 +1,10 @@
+
+public class TestGithub {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Test Github");
+
+	}
+
+}
---
+++ testgs/src/com/facebook/pages/TestGithub.java
@@ -0,0 +1,11 @@
+package com.facebook.pages;
+
+public class TestGithub {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Test Github");
+
+	}
+
+}
