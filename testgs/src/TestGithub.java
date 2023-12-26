105841a49c9f33720d3390fb4100d064109dd0f9
ganeshsaivolla <ganeshsaivolla@gmail.com>
2023-12-26 19:21:29 +0530

    new sysout

--- testgs/src/TestGithub.java
+++ testgs/src/TestGithub.java
@@ -3,8 +3,11 @@ public class TestGithub {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Test Github");
-		System.out.println("Test Github");
+		System.out.println("Test Github1");
+
+		System.out.println("Test Github2");
+
+		System.out.println("Test Github3");
 
 	}
 
